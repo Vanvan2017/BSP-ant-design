9fb6cc8e0ebca272d6e9bf63d032ad684b2280af
Yufan Chen <chenyufan@berkeley.edu>
2020-07-10 16:43:01 +0800

    permission 1.01

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -14,7 +14,7 @@ export const asyncRouterMap = [
     name: 'index',
     component: BasicLayout,
     meta: { title: 'menu.home' },
-    redirect: '/dashboard/workplace',
+    redirect: '/dashboard/mvo-workplace',
     children: [
 
       // dashboard
