f350157debae3b9b6c0d3a8f02f6e0c7ad21c187
87574 <875747176@qq.com>
2020-07-18 23:43:45 +0800

    -- finish cdm & par connection

--- src/views/BVO/MyOrders.vue
+++ src/views/BVO/MyOrders.vue
@@ -2,7 +2,7 @@
   <!-- <div>MyOrders</div> -->
   <a-result status="403" title="403" sub-title="Sorry, you don't have access to this page.">
     <template #extra>
-      <a-button type="primary" @click="Pay">
+      <a-button type="primary" @click="getExpress">
         Back Home
       </a-button>
     </template>
--- src/views/other/IconSelectorView.vue
+++ src/views/other/IconSelectorView.vue
@@ -4,15 +4,21 @@
 
     <a-divider />
     <p>测试 IconSelector 组件 v-model 功能</p>
-    <a-button @click="changeIcon('down')">改变 Icon-down</a-button>
+    <a-button @click="getParameterList">改变 Icon-down</a-button>
     <a-divider type="vertical" />
-    <a-button @click="changeIcon('cloud-download')">改变 Icon-cloud-download</a-button>
+    <a-button @click="removeParameter">改变 Icon-cloud-download</a-button>
   </a-card>
 </template>
 
 <script>
 import IconSelector from '@/components/IconSelector'
+import axios from 'axios'
 
+const request = axios.create({ // eslint-disable-line no-unused-vars
+  // API 请求的默认前缀
+  baseURL: 'http://localhost:9000/system/',
+  timeout: 6000 // 请求超时时间
+})
 export default {
   name: 'IconSelectorView',
   components: {
@@ -30,7 +36,80 @@ export default {
     },
     changeIcon (type) {
       this.currentSelectedIcon = type
-    }
+    },
+	getcdmList () {
+		request.post('CodeMasterController/getCdmList',
+		{
+			'userId':	3,
+			'rights':	1
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	},
+	addCdm () {
+		request.post('CodeMasterController/saveCdm',
+		{
+			'CdmCodeMasterDto': {
+				'codeType': 'string',
+				'codeVal': 'string',
+				'description': 'testCase',
+				'remark': 'string',
+				'typeCd': 'string'
+			},
+			'SysUserDto': {
+				'userId':	3
+			}
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	},
+	removeCdm () {
+		request.post('CodeMasterController/removeCdm',
+		{
+			'cdmId': '3'
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	},
+	getParameterList () {
+		request.post('parameterController/getParameterList',
+		{
+			'userId':	3,
+			'rights':	1
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	},
+	saveParameter () {
+		request.post('parameterController/saveParameter',
+		{
+			'ParParameterDto': {
+				'description': 'string',
+				'paramCd': 'string',
+				'paramValue': 'string',
+				'remark': 'stringtest'
+			},
+			'SysUserDto': {
+				'userId':	3
+			}
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	},
+	removeParameter () {
+		request.post('parameterController/removeParameter',
+		{
+			'parId': '3'
+		}).then(function (response) {
+			console.log('sdsd')
+			console.log(response)
+		})
+	}
   }
 }
 </script>
