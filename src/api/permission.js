974e2e691f226a3e277b088272b49ccb9f2cff4a
LSKReno <18236198930@163.com>
2020-07-22 16:02:32 +0800

    feat: 权限管理完成

---
+++ src/api/permission.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+
+const api = {
+  Role: '/system/role',
+  UpdateRole: '/system/role/update',
+  UpdatePermission: '/system/permission/update'
+}
+
+export default api
+
+export function getRoleList (parameter) {
+  return request({
+    url: api.Role,
+    method: 'post',
+    params: parameter
+  })
+}
+
+export function updateRole (parameter) {
+  return request({
+    url: api.UpdateRole,
+    method: 'post',
+    params: parameter
+  })
+}
+
+export function updatePermission (parameter) {
+  console.log(parameter)
+  return request({
+    url: api.UpdatePermission,
+    method: 'post',
+    params: parameter
+  })
+}
