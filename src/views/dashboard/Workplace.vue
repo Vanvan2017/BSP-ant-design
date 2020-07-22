6141cfb16b98cd810adbbbdd7e572f4b435369df
LSKReno <18236198930@163.com>
2020-07-22 20:47:09 +0800

    update: 小改动

--- src/views/dashboard/Workplace.vue
+++ src/views/dashboard/Workplace.vue
@@ -141,17 +141,11 @@
 <script>
 import { timeFix } from '@/utils/util'
 import { mapState } from 'vuex'
-import axios from 'axios'
+import { axios as request } from '@/utils/request'
+import storage from 'store'
 import { getRoleList, getServiceList } from '@/api/manage'
 // const DataSet = require('@antv/data-set')
 import { STable, Ellipsis } from '@/components'
-
-const request = axios.create({
-  // eslint-disable-line no-unused-vars
-  // API 请求的默认前缀
-  baseURL: 'http://localhost:9000/system/',
-  timeout: 6000 // 请求超时时间
-})
 
 const columns = [
   {
@@ -267,15 +261,15 @@ export default {
     getMVOInfo () {
       var app = this
       request
-        .post('CompanyInformationController/getCompanyInfo', {
+        .post('/system/CompanyInformationController/getCompanyInfo', {
           manBuyerId: 0,
-          userId: 4,
-          username: 'string'
+          userId: storage.get('userId'),
+          username: storage.get('username')
         })
         .then(function (response) {
           console.log('sdsd')
           console.log(response)
-          var data = response.data.content
+          var data = response.content
           if (data) {
             var type = ''
             if (data.gmcReportType === '1') {
@@ -308,13 +302,13 @@ export default {
       console.log(values)
       console.log(brandDto.brdId)
       request
-        .post('BrdBrandController/saveBrand', {
-          UserId: 4,
+        .post('/system/BrdBrandController/saveBrand', {
+          UserId: storage.get('userId'),
           BrdBrandDto: brandDto
         })
         .then(function (response) {
           console.log(response)
-          if (response.data.success) {
+          if (response.success) {
             app.$message.info('add success', 5)
             window.location.reload()
           } else {
@@ -340,8 +334,8 @@ export default {
       // 	nameEn: 'TaoBao',
       // 	remark: 'ohhhh'
       // }
-      // request.post('BrdBrandController/saveBrand', {
-      // 	UserId: 4,
+      // request.post('/system/BrdBrandController/saveBrand', {
+      // 	UserId: storage.get('userId'),
       // 	BrdBrandDto: brandDto
       // }).then(function (response) {
       // 	console.log(response)
@@ -359,13 +353,13 @@ export default {
         remark: item.remark
       }
       request
-        .post('BrdBrandController/removeBrand', {
-          UserId: 4,
+        .post('/system/BrdBrandController/removeBrand', {
+          UserId: storage.get('userId'),
           BrdBrandDto: brandDto
         })
         .then(function (response) {
           console.log(response)
-          if (response.data.success) {
+          if (response.success) {
             app.$message.info('delete success', 5)
             window.location.reload()
           } else {
@@ -380,9 +374,9 @@ export default {
       console.log('1232')
       console.log(this.MVOInfo)
       // console.log(this.data instanceof Array)
-      request.post('BrdBrandController/getBrandsList', this.MVOInfo).then(function (response) {
+      request.post('/system/BrdBrandController/getBrandsList', this.MVOInfo).then(function (response) {
         console.log(response)
-        response.data.content.forEach(item => {
+        response.content.forEach(item => {
           console.log(item)
           app.dataSource.push(item)
         })
